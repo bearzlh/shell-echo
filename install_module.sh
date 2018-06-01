87347a4d7db9f71e8b7f24d0b2496cc9a3b99982
bearzlh <bear.zheng@yunzhihui.com>
2018-06-01 11:33:38 +0000

    format

--- install_module.sh
+++ install_module.sh
@@ -26,6 +26,7 @@ PHP_SRC_DIR="/data/src/php/"
 
 #php installed dir with format: 5.3.29_zts
 PHP_INSTALLED_DIR="/data/software/php/"
+
 #extension source dir
 MODULE_DIR="/data/src/module7/"
 
@@ -34,6 +35,30 @@ MODULE_DIR="/data/src/module7/"
 
 #install extension
 install(){
+    #param process
+    param_module="$1"
+    param_php="$2"
+
+    #extension filter
+    if [ ! -z "param_module" ];then
+        MODULE_NAMES=`ls $MODULE_DIR|grep -E "$param_module"`
+    else
+        MODULE_NAMES="smart_agent"
+    fi
+
+    grep_version=
+    grep_zts=
+
+    #version and zts filter
+    if [ ! -z "$param_php" ];then
+        if [ -z "`echo $param_php | grep '-'`" ];then
+            grep_version=$param_php
+        else
+            grep_version=`echo $param_php|cut -d '-' -f1`
+            grep_zts=`echo $param_php|cut -d '-' -f2`
+        fi
+    fi
+
     #filter php and zts type
     TO_MAKE_LIST=`ls $PHP_SRC_DIR|grep -v "tar"|grep "$grep_version"`
     installed_dirs=`ls /data/software/php/ | grep "$grep_zts"`
@@ -154,6 +179,23 @@ install(){
     info "all tasks completed"
 }
 
+
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  get_file_modify_time
+#   DESCRIPTION:  get modify time of file
+#    PARAMETERS:  file_path
+#       RETURNS:  time
+#-------------------------------------------------------------------------------
+get_file_modify_time ()
+{
+    path="$1"
+    if [ ! -f $1 ];then
+        error "$1 not exists"
+    else
+        echo `stat $1 | sed -n "7p"|cut -d " " -f2`
+    fi
+}
+
 #---  FUNCTION  ----------------------------------------------------------------
 #          NAME:  restart
 #   DESCRIPTION:  restart php-fpm
@@ -163,7 +205,7 @@ install(){
 restart()
 {
     log "stop php-fpm..."
-    if [ `ps -ef|grep php-fpm|wc -l` -gt 1 ];then
+    if [ `ps -ef | grep php-fpm | wc -l` -gt 1 ];then
         killall php-fpm
     else
         log "php-fpm thread not exists"
@@ -174,38 +216,72 @@ restart()
     
     if [ ! -z "$result" ];then
         error "result-error=>php-fpm start message:$result"
+        exit
     else
         info "ok"
     fi
 }
 
-#extension name
-if [ ! -z "$2" ];then
-    MODULE_NAMES=$2
-else
-    MODULE_NAMES="smart_agent"
-fi
+#---  FUNCTION  ----------------------------------------------------------------
+#          NAME:  test
+#   DESCRIPTION:  test module
+#    PARAMETERS:  php version
+#       RETURNS:  
+#-------------------------------------------------------------------------------
+test ()
+{
+    param_version="$1"
 
-grep_version=
-grep_zts=
+    test_list=`ls $PHP_INSTALLED_DIR | grep -E "$1"`
 
-#version and zts filter
-if [ ! -z "$3" ];then
-    if [ -z "`echo $3 | grep '-'`" ];then
-        grep_version=$3
-    else
-        grep_version=`echo $3|cut -d '-' -f1`
-        grep_zts=`echo $3|cut -d '-' -f2`
-    fi
-fi
+    for php in $test_list; do
+        export PHP="$php"
+        source ~/.bashrc
+        log_path="/data/log/phpagent/trace_data.json"
+        restart
+
+        time_pre=`get_file_modify_time $log_path`
+        curl 'http://192.168.100.100/admin/student/index?sort=id&order=desc&offset=0&limit=10&_=1527844738037' -H 'Cookie: thinkphp_show_page_trace=0|0; thinkphp_show_page_trace=0|0; PHPSESSID=bui9a3omlap0chtt9k1nuin51j; XDEBUG_SESSION=IDEKEY; keeplogin=1%7C86400%7C1527931135%7C7e3e73afb96824b6c95fcc50f181ae36' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36' -H 'Content-Type: application/json' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Referer: http://192.168.100.100/admin/student?addtabs=1' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' --compressed >> /dev/null
+        time_end=`get_file_modify_time $log_path`
+
+        log $time_pre
+        log $time_end
+        if [ "$time_pre" != "$time_end" ];then
+            info "log updated ok"
+        else
+            error "log updated false"
+            continue
+        fi
+
+        cat $log_path | jq . >> /dev/null 2>&1
+        if [ $? != 0 ];then
+            error "json format error"
+            continue
+        else
+            info "format ok"
+        fi
+
+        count=$(cat $log_path | jq . | grep array | uniq | wc -l)
+        if [ $count != 1 ];then
+            error "blacknames filter error"
+        else
+            info "filter ok"
+        fi
+    done
+}
 
 #命令列表
 case $1 in
-    "install")
-        install 
+    "install"|"i")
+        install "$2" "$3"
         ;;
 
     "restart")
         restart
         ;;
+
+    "test")
+        test $2
+        ;;
+
 esac
--- install_php.sh
+++ install_php.sh
@@ -6,9 +6,6 @@ LIB='libjpeg-turbo-devel libmcrypt-devel libpng-devel libmcrypt-devel openssl-de
 #php源码目录，名称格式：php-5.3.29
 PHP_SRC_DIR="/data/src/php/"
 
-#待安装的php目录列表
-TO_INSALL_LIST=`ls $PHP_SRC_DIR | grep "tar.gz" | grep "$1"`
-
 #日志文件
 LOG=/data/log/php_install
 
@@ -55,13 +52,16 @@ checkEnv
 #备份so文件
 backUpSo()
 {
+    version=$1
+    main_version=`echo $1 | cut -d "." -f1`
+
     if [ ! -d $SO_DIR ];then
         mkdir -p $SO_DIR
     fi
     module_dir=${APACHE_DIR}modules
-    php_so=`ls $module_dir | grep php`
+    php_so="libphp${main_version}.so"
     if [ -f "$module_dir/$php_so" ];then
-        mv $module_dir/$php_so $SO_DIR${1}.so
+        mv $module_dir/$php_so $SO_DIR${version}.so
     fi
 }
 
@@ -71,18 +71,17 @@ runMake()
     #zts选项
     for zts in $zts_flag
     do
-        flags=`echo "$DEFAULT $zts" | sed "s/"$SPLIT"/ /g"  | sed 's/ \+/ /g'`
+        if [ `echo $version | cut -d "." -f1,2` == "5.3" ];then
+            DEFAULT=`echo $DEFAULT | sed -n "s/--with-apxs2=\\/data\\/software\\/apache\\/bin\\/apxs//p"`
+        fi
+       flags=`echo "$DEFAULT $zts" | sed "s/"$SPLIT"/ /g"  | sed 's/ \+/ /g'`
 
         suffix=$version
         if [[ ! -z `echo "$flags" | grep "zts"` ]];then
-            #flags="$flags $apax_flag"
             suffix=${suffix}_zts
-         else
+        else
             suffix=${suffix}_nts
-            if [ `echo $version | cut -d "." -f1,2` == "5.3" ];then
-                $flags="$flags $fpm_flag"
-            fi
-        
+
         fi
 
         log "make distclean"
@@ -90,47 +89,73 @@ runMake()
         log "./configure --prefix=${TARGET_DIR}${suffix} $flags"
         ./configure --prefix=${TARGET_DIR}${suffix} $flags >> $LOG 2>&1
         if [ $? != 0 ];then
-            error "configure error,continue,$verion\n"
+            error "result-error:configure for $verion;continue\n"
             continue;
         fi
         log "sed -i 's/-g /-g3 -gdwarf-2 /g' Makefile"
         sed -i 's/-g /-ggdb3 /g' Makefile
         log "make"
         if [ $? != 0 ];then
-            error "make error,continue,$verion\n"
+            error "result-error:make for $version;continue\n"
             continue;
         fi
         make >> $LOG 2>&1
+
         log "make install"
-        #如果已经安装，则删除
+        #remove php installed dir if exists
         if [ -d "${TARGET_DIR}${suffix}" ];then
+            log "rm -rf ${TARGET_DIR}${suffix}"
             rm -rf ${TARGET_DIR}${suffix}
         fi
-        #如果已经存在phpso文件，则删除
-        if [ ! -z "`ls ${APACHE_DIR}modules | grep php`" ];then
-            rm -rf ${APACHE_DIR}modules/libphp*.so
-        fi
+
+        #backup libphp[57].so if created
         make install >> $LOG 2>&1
         if [[ "$flags" == *apxs*  ]];then
             log "backUpSo $suffix"
             backUpSo $suffix
         fi
+
         log "php.ini-development ${TARGET_DIR}${suffix}/lib/php.ini"
         cp php.ini-development ${TARGET_DIR}${suffix}/lib/php.ini
-        info "successful $suffix with $flags"
+        info "result-success: $suffix with $flags"
     done
 }
 
 #遍历php版本并执行安装
-for php in $TO_INSALL_LIST
-do
-    cd $PHP_SRC_DIR
-    dirname=${php%.tar*}
-    if [ -d "$dirname" ];then
-        rm -rf $dirname
-    fi
-    tar zxf $php
-    version=`echo $dirname|cut -d '-' -f2`
-    cd $PHP_SRC_DIR$dirname
-    runMake "$version"
-done
+install ()
+{
+    #待安装的php目录列表
+    TO_INSALL_LIST=`ls $PHP_SRC_DIR | grep "tar.gz" | grep -E "$1"`
+
+    for php in $TO_INSALL_LIST
+    do
+        log "cd $PHP_SRC_DIR"
+        cd $PHP_SRC_DIR
+
+        dirname=${php%.tar*}
+        if [ -d "$dirname" ];then
+            log "remove $dirname if exists"
+            log "rm -rf $dirname"
+            rm -rf $dirname
+        fi
+
+        log "untar $php"
+        tar zxf $php
+        version=`echo $dirname|cut -d '-' -f2`
+
+        log "cd $PHP_SRC_DIR$dirname"
+        cd $PHP_SRC_DIR$dirname
+        runMake "$version"
+    done
+}
+
+case $1 in
+    "i" | "install")
+        install $2
+        ;;
+
+    *)
+        log "./install_php.sh (i)nstall"
+        ;;
+
+    esac
